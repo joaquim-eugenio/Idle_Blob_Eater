178e5fd53253c773fe9f6bf34993c6c05a394de7
joaquim-eugenio <joaquim@infinitygames.io>
2026-06-01 14:35:01 +0100

    [DONE]

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.idleblobeater.app',
+  appName: 'Idle Blob Eater',
+  webDir: 'dist'
+};
+
+export default config;
